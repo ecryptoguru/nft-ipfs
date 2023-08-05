5ca1e3e8669f8a60c1000ccd5345f9f4a71f8adc
Ankit Das <ecryptoguru@gmail.com>
2023-08-06 00:54:21 +0530

    AnkitPunks NFT Collection

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,28 @@
+const hre = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  // URL from where we can extract the metadata for a AnkitPunks
+  const metadataURL = "https://ipfs.io/ipfs/QmXVwNZfJEf3ZbfcqDJRAyWc5bfHzWbP4SnhPrwgxcLpxg/";
+  /*
+  DeployContract in ethers.js is an abstraction used to deploy new smart contracts,
+  so AnkitPunksContract here is a factory for instances of our AnkitPunks contract.
+  */
+  // here we deploy the contract
+ const AnkitPunksContract = await hre.ethers.deployContract("AnkitPunks", [
+   metadataURL
+ ]);
+
+  await AnkitPunksContract.waitForDeployment();
+
+ // print the address of the deployed contract
+  console.log("AnkitPunks Contract Address:", AnkitPunksContract.target);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
